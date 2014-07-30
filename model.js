d0080fd12f82f25efe9564046ee6207ef87f33ee
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2014-07-30 16:52:46 +0200

    Add check on tagli.

--- model.js
+++ model.js
@@ -1,9 +1,18 @@
 // this is the model representing the cuts
-var LavorazioneBuilder = function() {
+var LavorazioneBuilder = function(size) {
+    if (size === undefined) {
+        throw new Error('missing size parameter');
+    }
+
     return {
+        size: size,
         tagli: [],
 
         add: function(value, count) {
+            if ((value * count) > size) {
+                throw new Error('xxx');
+            }
+
             this.tagli.push({"value": value, "count": count});
         }
     }
