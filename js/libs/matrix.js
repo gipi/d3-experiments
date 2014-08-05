7e601aea43d75d19ce97b04595837898c814db9f
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2014-08-05 13:43:27 +0200

    Add module that defines matrix layout.

---
+++ js/libs/matrix.js
@@ -0,0 +1,50 @@
+define(["d3"], function(d3) {
+    return function(el, cols, rows) {
+        var m = {
+            el: el,
+            cols: cols,
+            rows: rows,
+            max: 20,
+            data: null,
+            svg: null,
+
+            init: function() {
+                this.svg = el.append('svg');
+            },
+
+            // matrix -> array
+            adapter: function(data) {
+                var objs = [];
+
+                for (var x = 0 ; x < this.cols ; x++) {
+                    for (var y = 0 ; y < this.rows ; y++) {
+                        objs.push({'x': x, 'y': y, 'value': data[x][y]});
+                    }
+                }
+
+                return objs;
+            },
+
+            // take a matrix as argument
+            update: function(data) {
+                var that = this;
+                var cx = function(d) {
+                    return d.x * that.max + ((that.max - d.value) / 2);
+                };
+                this.svg.selectAll('rect')
+                    .data(this.adapter(data))
+                        .enter().append('rect')
+                    .attr('x', cx)
+                    .attr('y', function(d) { return d.y * that.max + ((that.max - d.value) / 2); })
+                    .attr('width', function(d) { return d.value; })
+                    .attr('height', function(d) { return d.value; })
+                    .attr('fill', 'rgba(0, 0, 0, 1)')
+                                ;
+            }
+        }
+        m.init();
+
+        return m;
+    };
+
+});
